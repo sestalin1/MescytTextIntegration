40a054b94db8ff682367643800151af081425e19
sestalin <crjleonardo5@gmail.com>
2019-10-09 11:39:17 -0400

    Crud models in admin site

--- csvintegration/admin.py
+++ csvintegration/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from django.contrib import admin
+
+from .models import University
+from .models import Carreer
+
+admin.site.register(University)
+admin.site.register(Carreer)
