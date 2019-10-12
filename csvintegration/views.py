447bbb95136265b83bc5b930e37ad8f2ee496734
Juan Leonardo <crjleonardo5@gmail.com>
2019-10-12 00:18:39 -0400

    HTML upload

--- csvintegration/views.py
+++ csvintegration/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	data = {}
+	if "GET" == request.method:
+		return render(request, "index.html", data)
+    #return HttpResponse("Hello, world. You're at the polls index.")
